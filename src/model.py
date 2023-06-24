0e49258e2dfbfaabaa6653f0fb661f9f1c6dea23
Hoss Shakiba <hossshakiba@outlook.com>
2023-06-24 18:15:20 +0330

    Simplify the model for experiments

--- src/model.py
+++ src/model.py
@@ -26,15 +26,15 @@ class AutoEncoder(nn.Module):
             nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            ResidualBlock(64),
+            # ResidualBlock(64),
             nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            ResidualBlock(128),
+            # ResidualBlock(128),
             nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            ResidualBlock(256),
+            # ResidualBlock(256),
             nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1),
             nn.ReLU(inplace=True)
         )
@@ -45,15 +45,15 @@ class AutoEncoder(nn.Module):
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
             nn.ReLU(inplace=True),
-            ResidualBlock(64),
+            # ResidualBlock(64),
             nn.ConvTranspose2d(64, 3, kernel_size=3, stride=1, padding=1),
             nn.Sigmoid()
         )
 
-        self.unpool = nn.MaxUnpool2d(kernel_size=2, stride=2)
+        # self.unpool = nn.MaxUnpool2d(kernel_size=2, stride=2)
 
     def forward(self, x):
         x = self.encoder(x)
-        x = self.unpool(x)
+        # x = self.unpool(x)
         x = self.decoder(x)
         return x
