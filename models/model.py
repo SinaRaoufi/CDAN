d4f3dff6b915595c73fc9516d7d1ea7dc6ae6c1f
Karthikeya Reddy B S <120316681+Karthibs@users.noreply.github.com>
2025-03-07 21:06:17 +0530

    Update model.py

--- models/model.py
+++ models/model.py
@@ -85,47 +85,55 @@ class Model(BaseModel):
 
 
     def test_step(self):
-        """Test the model."""
-        path = os.path.join(self.model_path, self.model_name)
-        self.network.load_state_dict(torch.load(path))
-        self.network.eval()
+      """Test the model."""
+      path = os.path.join(self.model_path, self.model_name)
+      self.network.load_state_dict(torch.load(path))
+      self.network.eval()
 
-        psnr = PeakSignalNoiseRatio().to(self.device)
-        ssim = StructuralSimilarityIndexMeasure().to(self.device)
-        lpips = LearnedPerceptualImagePatchSimilarity(net_type='alex').to(self.device)
+      psnr = PeakSignalNoiseRatio().to(self.device)
+      ssim = StructuralSimilarityIndexMeasure().to(self.device)
+      lpips = LearnedPerceptualImagePatchSimilarity(net_type='alex').to(self.device)
 
-        with torch.no_grad():
-            test_loss = 0.0
-            test_psnr = 0.0
-            test_ssim = 0.0
-            test_lpips = 0.0
-            self.network.eval()
-            self.optimizer.zero_grad()
-            if self.is_dataset_paired:
-                for inputs, targets in tqdm(self.dataloader, desc='Testing...'):
-                    inputs, targets = inputs.to(self.device), targets.to(self.device)
+      all_outputs = []  # Accumulate outputs from all batches here
 
-                    outputs = self.network(inputs)
-                    if self.apply_post_processing:
-                        outputs = enhance_contrast(outputs, contrast_factor=1.12)
-                        outputs = enhance_color(outputs, saturation_factor=1.35)
-                    loss = self.criterion(outputs, targets)
-                    test_loss += loss.item()
-                    test_psnr += psnr(outputs, targets)
-                    test_ssim += ssim(outputs, targets)
-                    test_lpips += lpips(outputs, targets)
-            else:
-                for inputs in tqdm(self.dataloader, desc='Testing...'):
-                    inputs = inputs.to(self.device)
-                    outputs = self.network(inputs)
+      with torch.no_grad():
+          test_loss = 0.0
+          test_psnr = 0.0
+          test_ssim = 0.0
+          test_lpips = 0.0
+          self.network.eval()
+          self.optimizer.zero_grad()
+          
+          if self.is_dataset_paired:
+              for inputs, targets in tqdm(self.dataloader, desc='Testing...'):
+                  inputs, targets = inputs.to(self.device), targets.to(self.device)
+                  outputs = self.network(inputs)
+                  if self.apply_post_processing:
+                      outputs = enhance_contrast(outputs, contrast_factor=1.12)
+                      outputs = enhance_color(outputs, saturation_factor=1.35)
+                  loss = self.criterion(outputs, targets)
+                  test_loss += loss.item()
+                  test_psnr += psnr(outputs, targets)
+                  test_ssim += ssim(outputs, targets)
+                  test_lpips += lpips(outputs, targets)
+                  all_outputs.append(outputs)  # Append outputs of current batch
+          else:
+              for inputs in tqdm(self.dataloader, desc='Testing...'):
+                  inputs = inputs.to(self.device)
+                  outputs = self.network(inputs)
+                  all_outputs.append(outputs)  # Append outputs of current batch
 
-            test_loss = test_loss / len(self.dataloader)
-            test_psnr = test_psnr / len(self.dataloader)
-            test_ssim = test_ssim / len(self.dataloader)
-            test_lpips = test_lpips / len(self.dataloader)
+          if self.is_dataset_paired:
+              test_loss = test_loss / len(self.dataloader)
+              test_psnr = test_psnr / len(self.dataloader)
+              test_ssim = test_ssim / len(self.dataloader)
+              test_lpips = test_lpips / len(self.dataloader)
+              print(
+                  f'Test Loss: {test_loss:.4f}, Test PSNR: {test_psnr:.4f}, Test SSIM: {test_ssim:.4f}, Test LPIPS: {test_lpips:.4f}'
+              )
 
-            if self.is_dataset_paired:
-                print(
-                    f'Test Loss: {test_loss:.4f}, Test PSNR: {test_psnr:.4f}, Test SSIM: {test_ssim:.4f}, Test LIPIS: {test_lpips:.4f}')
-
-            self.generate_output_images(outputs, self.output_images_path)
+          # Concatenate outputs from all batches into one tensor
+          all_outputs = torch.cat(all_outputs, dim=0)
+          
+          # Generate output images from the concatenated tensor
+          self.generate_output_images(all_outputs, self.output_images_path)
